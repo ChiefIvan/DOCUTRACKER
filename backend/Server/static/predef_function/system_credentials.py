beea97e325aa812fd6c0e96c2e6867310ae54576
Ban <talonflame991@gmail.com>
2023-09-13 15:39:55 +0800

    Nothing Special

--- backend/Server/static/predef_function/system_credentials.py
+++
@@ -1,7 +0,0 @@
-from typing import Final
-
-EMAIL: Final[str] = "talonflame991@gmail.com"
-PASSWORD: Final[str] = "IvanPaulinDawang01112002"
-SECRET_KEY = "JustSomeRandomKey0111"
-SALT = "JustSomeRandomKey0111"
-CORS_LINK = "http://localhost:5173"
